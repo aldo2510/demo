6cf88302813048221bf1acd0e934786a26d269e3
aldo2510 <josealdotrucios@gmail.com>
2026-08-18 22:19:34 -0500

    feat: add customer request dto

---
+++ src/main/java/com/aldo/demo/api/dto/CreateCustomerRequest.java
@@ -0,0 +1,10 @@
+package com.aldo.demo.api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateCustomerRequest(
+        @NotBlank String documentNumber,
+        @NotBlank String fullName,
+        @NotBlank @Email String email) {
+}
